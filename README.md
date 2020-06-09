b1eef6546dbb22418850cda0686b38e67d42a0bf
Franck Moreira <franck.c.moreira@gmail.com>
2020-06-08 23:17:04 -0300

    Adding sources files

---
+++ README.md
@@ -0,0 +1,6 @@
+# springframework-demo
+Esse projeto é um demo básico de aplicação do conceito de IoC usando springframework.
+
+Para compilar e executar o binário basta executar o arquivo start.sh
+
+
---
+++ start.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# verifica se diretorio das libs springframework e caso não exista efetua o download das libs
+if [ ! -d "./lib" ]; then
+
+    if ! [ -x "$(command -v unzip)" ]; then
+        echo "Necessário instalar o unzip (sudo apt-get install zip unzip). Prosseguir (S/N)?"
+        read resposta
+        if [ $resposta = 'S' ] || [ $resposta = 's' ]; then
+            sudo apt-get update && sudo apt-get install zip unzip
+        else
+         exit 1
+        fi
+    fi   
+
+    mkdir .working
+
+    wget -O .working/spring-5.zip https://repo.spring.io/release/org/springframework/spring/5.2.6.RELEASE/spring-5.2.6.RELEASE-dist.zip
+
+    unzip -qq ./.working/spring-5.zip -d ./.working
+
+    mkdir lib
+
+    mv ./.working/spring-framework-5.2.6.RELEASE/libs/*.jar ./lib/
+
+    rm -rf ./.working
+fi
+
+# Efetua a leitura de todas as libs usadas e adiciona no arquivo classpath
+find ./lib -name *.jar -printf './lib/%f:' > .classpath.txt
+echo './src/main/resources/:./target/classes/' >> .classpath.txt
+
+# Lista todos os arquivos .java e guarda os caminhos dele em um arquivo
+find ./src/main/java -name "*.java" > ./sources.txt
+
+# Apaga diretório de classes antigo
+rm -rf ./target
+
+# Compila os arquivos java
+javac -cp @.classpath.txt -d ./target/classes/ @sources.txt
+
+# Apaga os arquivos auxiliares
+rm sources.txt
+
+java -cp @.classpath.txt com.demo.SpringHelloApp
